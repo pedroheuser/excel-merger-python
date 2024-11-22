a75a177b3c784dd9bfd79198c1e3b4de22118fb9
pedroheuser <pedrolheuserr@gmail.com>
2024-11-22 10:21:19 -0300

    tratamento para retirar horario, verificações e eliminação de duplicata

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import os 
 
-def consolidar_planilhas(diretorio, caminho_output):
+def consolidar_planilhas(diretorio_planilhas, caminho_output):
+    if not os.path.exists(diretorio_planilhas):
+        print(f"O diretório {diretorio_planilhas} não existe. Verifique o caminho.")
+        return None
+
     dataframe_finalizado = []
     colunas = {
         'Número de Inscrição do CNPJ': 'CNPJ',
@@ -11,20 +15,26 @@ def consolidar_planilhas(diretorio, caminho_output):
         'Validade do Certificado': 'Validade do Certificado'
     }
     
-    print(f"Arquivos no diretório {diretorio}: {os.listdir(diretorio)}")
+    print(f"Arquivos no diretório {diretorio_planilhas}: {os.listdir(diretorio_planilhas)}")
     
-    for arquivo in os.listdir(diretorio):
+    for arquivo in os.listdir(diretorio_planilhas):
         if arquivo.endswith(('.xlsx', '.xls')):
-            caminho_arquivo = os.path.join(diretorio, arquivo)
+            caminho_arquivo = os.path.join(diretorio_planilhas, arquivo)
             print(f"Lendo arquivo: {caminho_arquivo}")
             
             try:
                 df = pd.read_excel(caminho_arquivo)
                 print(f"Colunas encontradas: {df.columns}")
                 df.rename(columns=colunas, inplace=True)
+                
                 colunas_filtradas = [c for c in colunas.values() if c in df.columns]
                 df_filtrado = df[colunas_filtradas]
-            
+                
+                if "Validade do Certificado" in df_filtrado.columns:
+                    df_filtrado['Validade do Certificado'] = pd.to_datetime(
+                        df_filtrado['Validade do Certificado']
+                    ).dt.date
+
                 dataframe_finalizado.append(df_filtrado)
             except Exception as e:
                 print(f"Erro ao processar o arquivo {arquivo}: {e}")
@@ -34,10 +44,11 @@ def consolidar_planilhas(diretorio, caminho_output):
         return None
     
     df_final = pd.concat(dataframe_finalizado, ignore_index=True)
+    df_final = df_final.drop_duplicates(subset='CNPJ', keep='first')
     df_final.to_excel(caminho_output, index=False)
     return df_final
 
-diretorio_planilhas = "" 
-caminho_output = ""
+diretorio_planilhas = r"" 
+caminho_output = r""
 
 result = consolidar_planilhas(diretorio_planilhas, caminho_output)
